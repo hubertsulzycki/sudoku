ea6e3009ce17447df7d774cd3ffee3b053578b78
Hubert Sulżycki <sulzycki.hub@gmail.com>
2023-07-18 21:38:19 +0200

    tk inter entry

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -7,4 +7,12 @@ main_window.geometry("500x500")
 title=tk.Label(main_window, text="Sudoku",font=('Arial',20))
 title.pack(padx=15,pady=15)
 
+frame=tk.Frame(main_window)
+frame.pack()
+id_entry = tk.Entry(frame)
+id_entry.grid(row=1,column=0)
+
+for x in range(3):
+    for y in range(3):
+        pass
 main_window.mainloop()
