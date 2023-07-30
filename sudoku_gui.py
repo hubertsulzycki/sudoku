b5920eb6b39c5fb53a4c201837c2d4605d06656b
Hubert Sulżycki <sulzycki.hub@gmail.com>
2023-07-30 10:33:37 +0200

    added three buttons: check, restart, save

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -5,8 +5,7 @@ main_window.title("Sudoku")
 main_window.geometry("800x800")
 
 support_frame = tk.Frame(main_window, height=600, width=600, bg='green', padx=10, pady=10)
-support_frame.pack_configure(side=tk.TOP)  # Place at the top
-
+support_frame.pack()
 # Center support_frame both horizontally and vertically
 support_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
@@ -23,5 +22,17 @@ for x in range(9):
     for y in range(9):
         entries[x].append(tk.Entry(sudoku_frame, justify="center", width=2, font=('Arial', 24)))
         entries[x][y].grid(row=x, column=y)
+
+check_button=tk.Button(support_frame,text="Check",width=7, height=2)
+check_button.pack(padx=10,pady=10,side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+restart_icon=tk.PhotoImage(file="restart_icon.png")
+restart_icon=restart_icon.subsample(15,15)
+
+restart_button=tk.Button(support_frame,image=restart_icon,width=60, height=35)
+restart_button.pack(padx=10,pady=10,side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+save_button=tk.Button(support_frame,text="Save",width=7, height=2)
+save_button.pack(padx=10,pady=10,side=tk.LEFT, fill=tk.BOTH, expand=True)
 
 main_window.mainloop()
